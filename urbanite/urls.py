2e3a207b7347703c94834550fd6af99af3373362
“dhruvkhanna930” <dhruvkhanna930@gmail.com>
2024-06-18 11:21:07 +0530

    Customised admin.site properties

--- urbanite/urls.py
+++ urbanite/urls.py
@@ -8,3 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Urbanite Admin"
+admin.site.site_title = "Urbanite Admin Portal"
+admin.site.index_title = "Welcome to the Urbanite Portal"
